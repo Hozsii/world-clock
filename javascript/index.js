637b03664f2051b31dda99b5e43d065b2ec7dc77
Hozsii <131807273+Hozsii@users.noreply.github.com>
2024-07-03 22:07:23 +0100

    Added moment js functionality to the project

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,22 @@
+function updateTime() {
+  let parisElement = document.querySelector("#paris");
+  if (parisElement) {
+    let parisDateElement = parisElement.querySelector(".date");
+    let parisTimeElement = parisElement.querySelector(".time");
+    let parisTime = moment().tz("Europe/Paris");
 
+    parisDateElement.innerHTML = parisTime.format("MMMM DD, YYYY");
+    parisTimeElement.innerHTML = parisTime.format("HH:mm:ss ");
+  }
+
+  let sanFranciscoElement = document.querySelector("#san-francisco");
+  sanFrancisoDateElement = sanFranciscoElement.querySelector(".date");
+  sanFrancisoTimeElement = sanFranciscoElement.querySelector(".time");
+  let sanFrancicsoTime = moment().tz("America/Los_Angeles");
+
+  sanFrancisoDateElement.innerHTML = sanFrancicsoTime.format("MMMM DD, YYYY");
+  sanFrancisoTimeElement.innerHTML = sanFrancicsoTime.format("HH:mm:ss");
+}
+
+updateTime();
+setInterval(updateTime, 1000);
