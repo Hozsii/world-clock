f29143354f58afdf7b692947af5ca328b5d25b91
Hozsii <131807273+Hozsii@users.noreply.github.com>
2024-07-05 12:50:22 +0100

    Added functionalityto the select

--- javascript/index.js
+++ javascript/index.js
@@ -10,13 +10,34 @@ function updateTime() {
   }
 
   let sanFranciscoElement = document.querySelector("#san-francisco");
-  sanFrancisoDateElement = sanFranciscoElement.querySelector(".date");
-  sanFrancisoTimeElement = sanFranciscoElement.querySelector(".time");
-  let sanFrancicsoTime = moment().tz("America/Los_Angeles");
+  if (sanFranciscoElement) {
+    sanFrancisoDateElement = sanFranciscoElement.querySelector(".date");
+    sanFrancisoTimeElement = sanFranciscoElement.querySelector(".time");
+    let sanFrancicsoTime = moment().tz("America/Los_Angeles");
 
-  sanFrancisoDateElement.innerHTML = sanFrancicsoTime.format("MMMM DD, YYYY");
-  sanFrancisoTimeElement.innerHTML = sanFrancicsoTime.format("HH:mm:ss");
+    sanFrancisoDateElement.innerHTML = sanFrancicsoTime.format("MMMM DD, YYYY");
+    sanFrancisoTimeElement.innerHTML = sanFrancicsoTime.format("HH:mm:ss");
+  }
+}
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+  <div class="city">
+    <div>
+      <h2>${cityName}</h2>
+      <div class="date">${cityTime.format("MMMM	Do YYYY")}</div>
+    </div>
+    <div class="time">${cityTime.format("HH:mm:ss")} 
+  `;
 }
 
 updateTime();
 setInterval(updateTime, 1000);
+
+let citySelectElement = document.querySelector("#city");
+
+citySelectElement.addEventListener("change", updateCity);
